e94c9ffd68af47c5266f76eb49f4003829c2e824
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-03 12:24:24 +0530

    adding angular materials and home component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,15 +4,60 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
+import{
+  MatTableModule,
+} from '@angular/material/table';
+ import{
+  MatButtonModule,
+ } from '@angular/material/button';
+
+ import{
+  MatToolbarModule,
+ }from '@angular/material/toolbar';
+
+ import{
+  MatDatepickerModule,
+ }from '@angular/material/datepicker';
+
+ import{
+  MatInputModule,
+ }from '@angular/material/input';
+
+ import{
+  MatFormFieldModule,
+ }from '@angular/material/form-field';
+
+ import {
+  MatProgressSpinnerModule,
+ }from '@angular/material/progress-spinner';
+
+ import {
+  MatNativeDateModule,
+ }from '@angular/material/core';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
